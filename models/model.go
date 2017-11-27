8396af5d147e22688ca7eddaedb2e26fbd4ea222
aceld <danbing_at@163.com>
2017-11-27 11:04:12 +0800

    修正如果不添加name导致第二个用户无法注册问题

--- models/model.go
+++ models/model.go
@@ -9,7 +9,7 @@ import (
 /* 用户 table_name = user */
 type User struct {
 	Id            int           `json:"user_id"`                       //用户编号
-	Name          string        `orm:"size(32);unique"  json:"name"`   //用户昵称
+	Name          string        `orm:"size(32)"  json:"name"`          //用户昵称
 	Password_hash string        `orm:"size(128)" json:"password"`      //用户密码加密的
 	Mobile        string        `orm:"size(11);unique"  json:"mobile"` //手机号
 	Real_name     string        `orm:"size(32)" json:"real_name"`      //真实姓名

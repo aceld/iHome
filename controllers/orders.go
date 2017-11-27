f015d26ecad30122a1e904be785eb8bc3f7f4775
aceld <danbing_at@163.com>
2017-11-27 16:23:39 +0800

    添加用户评论功能

--- controllers/orders.go
+++ controllers/orders.go
@@ -17,6 +17,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/cache"
+	_ "github.com/astaxie/beego/cache/redis"
 	"github.com/astaxie/beego/orm"
 	"iHome/models"
 	"iHome/utils"
@@ -33,7 +35,7 @@ type OrderRequest struct {
 type OrderResp struct {
 	Errno  string      `json:"errno"`
 	Errmsg string      `json:"errmsg"`
-	Data   interface{} `json:"data"`
+	Data   interface{} `json:"data,omitempty"`
 }
 
 type OrdersController struct {
@@ -202,4 +204,95 @@ func (this *OrdersController) OrderStatus() {
 
 // /orders/:id/comment [PUT]
 func (this *OrdersController) OrderComment() {
+	rep := OrderResp{Errno: utils.RECODE_OK, Errmsg: utils.RecodeText(utils.RECODE_OK)}
+	defer this.RetData(&rep)
+
+	//获得用户id
+	user_id := this.GetSession("user_id").(int)
+
+	//得到订单id
+	order_id := this.Ctx.Input.Param(":id")
+
+	//获得参数
+	var req map[string]interface{}
+	if err := json.Unmarshal(this.Ctx.Input.RequestBody, &req); err != nil {
+		rep.Errno = utils.RECODE_REQERR
+		rep.Errmsg = utils.RecodeText(rep.Errno)
+		return
+	}
+
+	comment := req["comment"].(string)
+	//检验评价信息是否合法 确保不为空
+	if comment == "" {
+		rep.Errno = utils.RECODE_PARAMERR
+		rep.Errmsg = utils.RecodeText(rep.Errno)
+		return
+	}
+
+	//查询数据库，订单必须存在，订单状态必须为WAIT_COMMENT待评价状态
+	order := models.OrderHouse{}
+	o := orm.NewOrm()
+	if err := o.QueryTable("order_house").Filter("id", order_id).Filter("status", models.ORDER_STATUS_WAIT_COMMENT).One(&order); err != nil {
+		rep.Errno = utils.RECODE_DATAERR
+		rep.Errmsg = utils.RecodeText(rep.Errno)
+		return
+	}
+
+	//关联查询order订单所关联的user信息
+	if _, err := o.LoadRelated(&order, "User"); err != nil {
+		rep.Errno = utils.RECODE_DATAERR
+		rep.Errmsg = utils.RecodeText(rep.Errno)
+		return
+	}
+
+	//确保订单所关联的用户和该用户是同一个人
+	if user_id != order.User.Id {
+		rep.Errno = utils.RECODE_DATAERR
+		rep.Errmsg = "该订单并不属于本人"
+		return
+	}
+
+	//关联查询order订单所关联的House信息
+	if _, err := o.LoadRelated(&order, "House"); err != nil {
+		rep.Errno = utils.RECODE_DATAERR
+		rep.Errmsg = utils.RecodeText(rep.Errno)
+		return
+	}
+
+	house := order.House
+	//	fmt.Printf("=========== > house = %+v\n", house)
+
+	//更新order的status为COMPLETE
+	order.Status = models.ORDER_STATUS_COMPLETE
+	order.Comment = comment
+
+	//将房屋订单成交量+1
+	house.Order_count++
+
+	//将order和house更新数据库
+	if _, err := o.Update(&order, "status", "comment"); err != nil {
+		beego.Error("update order status, comment error, err = ", err)
+		rep.Errno = utils.RECODE_DATAERR
+		rep.Errmsg = utils.RecodeText(rep.Errno)
+		return
+	}
+
+	if _, err := o.Update(house, "order_count"); err != nil {
+		beego.Error("update house order_count error, err = ", err)
+		rep.Errno = utils.RECODE_DATAERR
+		rep.Errmsg = utils.RecodeText(rep.Errno)
+		return
+	}
+
+	//将house_info_[house_id]的缓存key删除 （因为已经修改订单数量）
+	cache_conn, err := cache.NewCache("redis", `{"key": "ihome_go", "conn":"127.0.0.1:6380", "dbNum":"8"}`)
+	if err != nil {
+		beego.Debug("connect cache error")
+	}
+	house_info_key := "house_info_" + strconv.Itoa(house.Id)
+	if err := cache_conn.Delete(house_info_key); err != nil {
+		beego.Error("delete ", house_info_key, "error , err = ", err)
+	}
+
+	return
 }
